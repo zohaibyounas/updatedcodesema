cfdaacdc6db4290d58435d6392e70cd5b3a5f85a
zohaibyounas <zohaiby737@gmail.com>
2024-10-29 15:18:42 +0500

    updated

--- app/_components/Navbar.js
+++ app/_components/Navbar.js
@@ -6,7 +6,7 @@ import { handleScrollTo } from "../_helpers/scroll";
 export default function Navbar({ navItems }) {
   return (
     <ul className="flex items-center gap-12 text-xl xl:text-3xl text-black ">
-      {navItems.map((item) => (
+      {navItems?.map((item) => (
         <li
           key={item}
           className="cursor-pointer transition-all hover:text-stone-400"
--- app/about/page.js
+++ app/about/page.js
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-
+import Navbar from "../_components/Navbar";
 export default function AboutUs() {
   return (
     <div
